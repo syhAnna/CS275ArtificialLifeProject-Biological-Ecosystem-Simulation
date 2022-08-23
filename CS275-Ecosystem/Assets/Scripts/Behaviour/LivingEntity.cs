4e8db1d492448562bbb9a8f5da33402bb8595277
Yuhan Shao <shaoyuhan96117@gmail.com>
2022-08-23 17:36:42 -0400

    Add files via upload

---
+++ CS275-Ecosystem/Assets/Scripts/Behaviour/LivingEntity.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+
+public class LivingEntity : MonoBehaviour {
+    float amountRemaining = 1;
+    const float consumeSpeed = 8;
+
+    public int colourMaterialIndex;
+    public Species species;
+    public Material material;
+
+    public Coord coord;
+    //
+    [HideInInspector]
+    public int mapIndex;
+    [HideInInspector]
+    public Coord mapCoord;
+
+    protected bool dead;
+
+    public virtual void Init (Coord coord) {
+        this.coord = coord;
+        transform.position = Environment.tileCentres[coord.x, coord.y];
+
+        // Set material to the instance material
+        var meshRenderer = transform.GetComponentInChildren<MeshRenderer> ();
+        for (int i = 0; i < meshRenderer.sharedMaterials.Length; i++)
+        {
+            if (meshRenderer.sharedMaterials[i] == material) {
+                material = meshRenderer.materials[i];
+                break;
+            }
+        }
+    }
+
+    protected virtual void Die (CauseOfDeath cause) {
+        if (!dead) {
+            dead = true;
+            Environment.RegisterDeath (this);
+            Destroy (gameObject);
+        }
+    }
+
+    // add Consume function from Plant
+    public float Consume (float amount) {
+        float amountConsumed = Mathf.Max (0, Mathf.Min (amountRemaining, amount));
+        amountRemaining -= amount * consumeSpeed;
+
+        transform.localScale = Vector3.one * amountRemaining;
+
+        if (amountRemaining <= 0) {
+            Die (CauseOfDeath.Eaten);
+        }
+
+        return amountConsumed;
+    }
+
+    public float AmountRemaining {
+        get {
+            return amountRemaining;
+        }
+    }
+}
---
+++ CS275-Ecosystem/Assets/Scripts/Behaviour/Rabbit.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rabbit : Animal {
+    float amountRemaining = 1;
+    const float consumeSpeed = 1;
+
+    public static readonly string[] GeneNames = { "A", "B" };
+
+    public float Consume (float amount) {
+        float amountConsumed = Mathf.Max (0, Mathf.Min (amountRemaining, amount));
+        amountRemaining -= amount * consumeSpeed;
+
+        transform.localScale = Vector3.one * amountRemaining;
+
+        if (amountRemaining <= 0) {
+            Die (CauseOfDeath.Eaten);
+        }
+
+        return amountConsumed;
+    }
+
+    public float AmountRemaining {
+        get {
+            return amountRemaining;
+        }
+    }
+}
